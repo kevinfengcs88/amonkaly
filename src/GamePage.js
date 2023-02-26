9684cbf15c125a12c5f4d0a95f53bd8714942e71
Eric Xuan <eric@nbp-109-158.nbp.ruw.rutgers.edu>
2023-02-26 15:34:55 -0500

    updated players

--- src/GamePage.js
+++ src/GamePage.js
@@ -4,6 +4,7 @@ import React, {useState} from 'react'
 import { GameCard } from './components/GameCard';
 import { io } from 'socket.io-client';
 
+
 const socket = io('http://localhost:5000');
 
 const GamePage = () => {
@@ -15,13 +16,43 @@ const GamePage = () => {
 
   socket.on('connect', () => {
     socket.emit('socket-connection-event', socket.id);
+
   });
+
+  socket.on('p1-turn', (info) => {
+    
+  });
+
+  const player = 1;
+
+  const [p1, setP1] = useState([]);
+  const [p2, setP2] = useState([]);
+  const [p3, setP3] = useState([]);
+  const [p4, setP4] = useState([]);
+
+  const players = [p1,p2,p3,p4];
 
   const handleClickPile = () => {
     socket.emit('deck-click-event', 'Deck was clicked');
 
     const newCard = Math.floor(Math.random() * 5);
-    setPlayer(player => [...player, newCard]);
+    switch (player) {
+      case 1:
+        setP1(p1 => [...p1, newCard]);
+        break;
+      case 2:
+        setP2(p2 => [...p2, newCard]);
+        break;
+      case 3:
+        setP3(p3 => [...p3, newCard]);
+        break;
+      case 4:
+        setP4(p4 => [...p4, newCard]);
+        break;
+            
+    }
+
+    setP1(p1 => [...p1, newCard]);
   }
 
   const handleClickCard = () => {
@@ -34,12 +65,14 @@ const GamePage = () => {
       <div className="App-header">
         <h2 className="h2">Game Room</h2>
         <div className="table" id="table" z='1'>
-            <div className='card card-p1'><GameCard/></div>
-            <div className='card card-p2'><GameCard/></div>
-            <div className='card card-p3'><GameCard/></div>
-            {console.log(player)}
+            <div className='card card-p1' onClick={handleClickCard}><GameCard/></div>
+            
+            <div className='card card-p2' onClick={handleClickCard}><GameCard/></div>
+            
+            <div className='card card-p3' onClick={handleClickCard}><GameCard/></div>
+
             {player.map((cardSymbol) => (
-                <div className='card card-player' id={player.length} z-index={player.length+10} onClick={handleClickCard}>
+                <div className='card card-p4' id={player.length} z-index={player.length+10}>
                   <GameCard symbol={cardSymbol}/>
                 </div>
             ))}
