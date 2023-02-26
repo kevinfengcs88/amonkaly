2139df45c37b993ae1c1094bcc0731bf4951ecef
Eric Xuan <eric@nbp-109-158.nbp.ruw.rutgers.edu>
2023-02-26 11:11:25 -0500

    added symbol randomization

--- src/GamePage.js
+++ src/GamePage.js
@@ -8,12 +8,13 @@ function GamePage() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="h1">Game Room</h1>
+        <h2 className="h2">Game Room</h2>
         <div className="table">
           <div class='card card-p1'><GameCard/></div>
           <div class='card card-p2'><GameCard/></div>
           <div class='card card-p3'><GameCard/></div>
-          <div class='card card-p4'><GameCard/></div>
+          <div class='card card-player'><GameCard/></div>
+          <div class='card card-pile'><GameCard cardType='pile'/></div>
         </div>
       </header>
 
--- src/components/GameCard/index.js
+++ src/components/GameCard/index.js
@@ -10,28 +10,50 @@ import { ReactComponent as Logo4 } from "../../assets/red-dots.svg";
 import { ReactComponent as Logo5 } from "../../assets/yellow-square.svg";
 
 const GameCard = ({
-
+    cardType
 }) => {
-    return (
-        <Container
-            sx={{
-                p: 1.5,
-                border: 2,
-                width: 120,
-                direction: 'column',
-                borderColor: 'black',
-                backgroundColor: 'white'
-            }}
-        >
+
+    let design;
+    const logos = [<Logo1 />, <Logo2 />, <Logo3 />, <Logo4 />, <Logo5 />];
+
+    if (cardType != 'pile') {
+        design = <div>
             <div className='card-word'>Word</div>
             <br/>
-            <div>
-                <SvgIcon>
-                    <Logo1 />
+            <div alignItems='center'>
+                <SvgIcon
+                    className='symbol'
+                    sx={{
+                        borderColor: 'black'
+                    }}>
+                    {logos[Math.floor(Math.random()*5)]}
                 </SvgIcon>
             </div>
-            
-        </Container>
+        </div>
+    }
+    else {
+        design = <div className='card-back'>
+            Amonkaly
+        </div>
+    }
+
+    const card = <Container
+        sx={{
+            p: 1.2,
+            border: 2,
+            width: 120,
+            height: 150,
+            borderRadius: 2.5,
+            direction: 'column',
+            borderColor: 'black',
+            backgroundColor: 'white',
+            justify: 'center'
+        }}>
+        {design}
+    </Container>
+
+    return (
+        card
     );
 }
 
