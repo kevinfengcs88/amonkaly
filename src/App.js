44b112ef08a54fec6e11478aa5b99470b048f654
Giyuu <tranhoanglong1904@gmail.com>
2023-02-25 20:59:51 -0500

    socket on client

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from './Home';
 import GamePage from './GamePage';
+import { io } from 'socket.io-client';
+
+let socket = io();
 
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  useEffect(()=> {
+    socket.on('connect', () => {
+      setIsConnected(true);
+    });
+  
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+  
+    console.log('Socket connection is: ' + isConnected);
+
+    return ()=> {
+      socket.off('connect');
+      socket.off('disconnect');
+    }
+  }, []);
   return (
     <>
       <BrowserRouter>
